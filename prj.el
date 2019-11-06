f1c1fc8fde9fed034ceec5ec2885b14848d9972e
Lei Zhao <LeiZh26@gmail.com>
2019-11-07 00:13:15 +0800

    Add some new files
    Also update prj.el and .gitingore

--- prj.el
+++ prj.el
@@ -1,4 +1,9 @@
 (jdee-project-file-version "1.0")
 (jdee-set-variables
  '(jdee-run-working-directory ".")
- '(jdee-compile-option-classpath (quote ("."))))
+ '(jdee-compile-option-classpath (quote (".")))
+ '(jdee-built-class-path (quote (".")))
+ '(jdee-compile-option-sourcepath (quote (".")))
+ '(jdee-sourcepath (quote (".")))
+ '(jdee-xref-store-prefixes (quote ("components" "layout")))
+ '(jdee-global-classpath (quote ("."))))
